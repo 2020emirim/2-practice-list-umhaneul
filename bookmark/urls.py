986b0778b980e79fdab9041babf2eec562f91587
umhaneul <asd7g123@naver.com>
2020-05-22 15:32:00 +0900

    models get_absolute_url() detail

--- bookmark/urls.py
+++ bookmark/urls.py
@@ -7,6 +7,6 @@ app_name = 'bookmark'
 urlpatterns = [
     path('', BookmarkList.as_view(), name='list'), #{% url 'bookmark:list' %}
     path('add/', BookmarkCreateView.as_view(), name='add'),
-    path('detail/<int:pk>/', BookmarkDetailView.as_view(), name='detial'),
+    path('detail/<int:pk>/', BookmarkDetailView.as_view(), name='detail'),
     path('update/<int:pk>/', BookmarkUpdateView.as_view(), name='update')
 ]
